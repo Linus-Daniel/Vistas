5ba1c947489ce2766a2629ec3a1448fa787a8e93
Linus-Daniel <vandulinus@gmail.com>
2025-08-07 16:43:10 +0100

    initial commit

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -3,31 +3,19 @@ import { getServerSession } from "next-auth";
 import api from "@/lib/axiosInstance";
 import { cookies } from "next/headers";
 import RenderOrder from "@/components/admin/OrderWrapper";
-import Order from "@/models/Order";
 
 async function getOrders() {
-  const session = await getServerSession();
-  const user = session?.user;
   try {
     const cookiesStore = await cookies();
     const cookieHeader = cookiesStore.toString();
 
-    if (user?.role != "admin") {
-      return;
-    }
+    const response = await api.get("/orders", {
+      headers: {
+        Cookie: cookieHeader,
+      },
+    });
 
-    const orderData = Order.find({});
-    console.log(orderData, "my orders");
-    // orders = orderData
-
-    // const response = await api.get("/orders", {
-    //   headers: {
-    //     Cookie: cookieHeader,
-    //   },
-    // });
-
-    // return response.data;
-  
+    return response.data;
   } catch (error) {
     console.error("Error fetching orders:", error);
     return [];
@@ -37,8 +25,7 @@ async function getOrders() {
 async function page() {
   const session = await getServerSession();
   const user = session?.user;
-  const ordersData = await getOrders();
-  
+
   // Check if user is admin (adjust this logic based on your auth setup)
   if (!session || !user) {
     return (
@@ -53,12 +40,14 @@ async function page() {
     );
   }
 
+  const ordersData = await getOrders();
 
-  // console.log(ordersData);
+  console.log((ordersData))
 
   return (
     <div>
-      {/* <RenderOrder initialOrders={ordersData} /> */}
+      <RenderOrder initialOrders={ordersData}/>
+      
     </div>
   );
 }
